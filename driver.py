7adeb3055e8f773c55d2668ab53e6136526af351
Krish Chowdhary <krishchow.10@gmail.com>
2019-09-20 00:06:43 -0400

    adding form filling

--- driver.py
+++ driver.py
@@ -4,8 +4,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 SyllabiDirectory = 'Syllabi/'
+SessionSelectID = 'session_cd'
+DepartmentSelectID = 'department_id'
 
 def gatherSyllabi(browser, username, password):
     userName = WebDriverWait(browser,60).until(EC.presence_of_element_located((By.ID, 'username')))
@@ -13,10 +16,13 @@ def gatherSyllabi(browser, username, password):
     userName.send_keys(username)
     passwordName.send_keys(password)
     passwordName.submit()
+    WebDriverWait(browser,60).until(EC.presence_of_element_located((By.ID, 'fippa')))
+    return
 
-
-
-
+def setSelect(select_id, option_value):
+    select = Select(driver.find_element_by_id(select_id))
+    select.select_by_value(option_value)
+    return
 
 if __name__ == '__main__':
     username = input("Username: ")
@@ -38,8 +44,15 @@ if __name__ == '__main__':
         value += '1'
     elif term == 'Summer':
         value += '5'
-    browser = createBrowser(url='https://student.utm.utoronto.ca/CourseInfo/index.php?session_cd=20199&department_id=7',
+    departments = ['7', '27', '38']
+    browser = createBrowser(url='https://student.utm.utoronto.ca/CourseInfo/index.php',
                             headless=False,
                             blockImages=False,
                             hideConsole=False)
-   
+    gatherSyllabi(browser)
+    setSelect(SessionSelectID, value)
+    for department in departments:
+        setSelect(DepartmentSelectID, department)
+        WebDriverWait(browser,60).until(EC.presence_of_element_located((By.XPATH, "//input[@value='Search']")))
+    
+            
